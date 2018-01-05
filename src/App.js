a3a6a61d71d6ec52169f0c877d86f3e50c50bf30
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-01-05 07:46:59 +0000

    egghead 13

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
 
-import {bindActionCreators} from 'redux'
 import {updateCurrent} from './reducers/todo'
 
 class App extends Component {
@@ -29,7 +28,7 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => state
-const mapDispatchToProps = (dispatch) => bindActionCreators({updateCurrent}, dispatch)
-const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App)
-export default ConnectedApp
+export default connect(
+  (state) => state, 
+  {updateCurrent}
+)(App)
