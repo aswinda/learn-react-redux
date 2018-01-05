6fb6baf8b8902673975fae02a7c7c059032c8793
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-01-05 10:06:42 +0000

    egghead 17

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {updateCurrent} from '../reducers/todo'
-
-const TodoForm = (props) => {
-  console.log('Rendering Form')
-  const {currentTodo, updateCurrent} = props
-  const handleInputChange = (evt) => {
-    const val = evt.target.value
-    updateCurrent(val)
+import {updateCurrent, saveTodo} from '../reducers/todo'
+  
+class TodoForm extends Component {
+  handleInputChange = (evt) => {
+      const val = evt.target.value
+      this.props.updateCurrent(val)
   }
-  return (
-    <form>
-      <input type="text" 
-        onChange={handleInputChange}
-        value={currentTodo} />
-    </form>
-  )
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    this.props.saveTodo(this.props.currentTodo)
+  }
+
+  render() {
+    const {currentTodo} = this.props
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" 
+          onChange={this.handleInputChange}
+          value={currentTodo} />
+      </form>
+    )
+  }
 }
 
 export default connect(
   (state) => ({currentTodo: state.currentTodo}),
-  {updateCurrent}
+  {updateCurrent, saveTodo}
 )(TodoForm)
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,4 +1,4 @@
-import {getTodos} from '../lib/todoServices'
+import {getTodos, createTodo} from '../lib/todoServices'
 const initState = {
 	todos: [],
 	currentTodo: 'temp'
@@ -12,10 +12,19 @@ export const updateCurrent = (val) => ({type: CURRENT_UPDATE, payload:val})
 
 export const loadTodos = (todos) => ({type: TODOS_LOAD, payload: todos})
 
+export const addTodo = (todo) => ({type: TODO_ADD, payload: todo})
+
 export const fetchTodos = () => {
 	return (dispatch) => {
 		getTodos()
 			.then(todos => dispatch(loadTodos(todos)))
+	}
+}
+
+export const saveTodo = (name) => {
+	return (dispatch) => {
+		createTodo(name)
+			.then(res => dispatch(addTodo(res)))
 	}
 }
 
